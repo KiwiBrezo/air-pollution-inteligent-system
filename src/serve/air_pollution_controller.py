14774efe4aa85ef79328ba7ea567e4260f0f9405
jBreznar <kiwi.breznar@gmail.com>
2023-04-15 21:10:33 +0200

    Popravek CORS-a na API-ju.

--- src/serve/air_pollution_controller.py
+++ src/serve/air_pollution_controller.py
@@ -25,16 +25,9 @@ class WeatherData(BaseModel):
     pm25: int
 
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
